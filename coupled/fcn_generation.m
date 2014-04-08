64fd0bd60896017df5dfc15db69c09e57397807c
Paweł Bogner <pawelbogner@gmail.com>
2014-04-08 22:04:58 +0200

    poprawki w generacji funkcji: inv, itp.

--- coupled/fcn_generation.m
+++ coupled/fcn_generation.m
@@ -1,18 +1,16 @@
 %% variables
 n=5; %dim q
-x_one=sym('x_one', [n 1]);
-x_two=sym('x_two', [n 1]);
-x=[x_one; x_two];
+x=sym('x', [2*n 1]);
 m=2;
 %u=sym('u', [m 1]);
-s=m*(2*2+1);
+s=m*(2*3+1);
 
 lam=sym('lam', [s 1]);
 syms t;
 
 %%parameters
 a=0.730;
-b=0.530;
+b=0.350;
 R=0.127;
 mp=21.107;  
 mw=2.380;
@@ -20,45 +18,49 @@ Iw11=0.015;
 Iw33=0.009;
 Ip33=1.991;
 ap1=0.377;
-ap2=0;
+ap2=0.008;
 gr=9.81;
-T=20;
+T_h=4;
 
 %% Q elements
 Q11=mp+4*mw;
 Q22=Q11;
 
-Q44=Iw33;
+Q44=2*Iw33;
 Q55=Q44;
-Q66=Q55;
-Q77=Q66;
 
-Q33= Ip33 + mp*(ap1^2+ap2^2) + 4*(Iw11 + mw*b^2) + 2*mw*a^2;
+Q33= Ip33 + mp*(ap1.^2+ap2.^2) + 4*(Iw11 + mw*b.^2) + 2*mw*a.^2;
 
-Q13 = -mp*(ap1*sin(x_one(3)/a) + ap2*cos(x_one(3)/a)) - 2*mw*a*sin(x_one(3)/a);
-Q23 =  mp*(ap1*cos(x_one(3)/a) - ap2*sin(x_one(3)/a)) + 2*mw*a*cos(x_one(3)/a);
+Q13 = -mp*(ap1*sin(x(3)./a) + ap2*cos(x(3)./a)) - 2*mw*a*sin(x(3)./a);
+Q23 =  mp*(ap1*cos(x(3)./a) - ap2*sin(x(3)./a)) + 2*mw*a*cos(x(3)./a);
 
 %% P matrix
-P = [Q11   0     Q13/a   0       0;
-     0     Q22   Q23/a   0       0;
-     Q13/a Q23/a Q33/a^2 0       0;
-     0     0     0       Q44/R^2 0;
-     0     0     0       0       Q55/R^2];
+P = [Q11    0      Q13./a   0        0;
+     0      Q22    Q23./a   0        0;
+     Q13./a Q23./a Q33./a.^2 0        0;
+     0      0      0        Q44./R.^2 0;
+     0      0      0        0        Q55./R.^2];
  
  %% D matrix
- D = x(8)^2/a^2*([-Q23; Q13; 0; 0; 0]);
+ D = x(8).^2./a.^2*([-Q23; Q13; 0; 0; 0]);
  
  %% H matrix
- H = [-sin(x_one(3)/a) cos(x_one(3)/a)    0  0   0;
-      -sin(x_one(3)/a) cos(x_one(3)/a)    1  0   0;
-       cos(x_one(3)/a) sin(x_one(3)/a) -b/a -1   0;
-       cos(x_one(3)/a) sin(x_one(3)/a)  b/a  0  -1];
+  
+   H1=[-sin(x(3)./a) cos(x(3)./a)    0  0   0];
+   H2=[-sin(x(3)./a) cos(x(3)./a)    1  0   0];
+   H3=[ cos(x(3)./a) sin(x(3)./a) -b/a -1   0];
+   H4=[ cos(x(3)./a) sin(x(3)./a)  b/a  0  -1];
+   
+   H1T=[-sin(x(3)./a); cos(x(3)./a);    0;  0;   0];
+   H2T=[-sin(x(3)./a); cos(x(3)./a);    1;  0;   0];
+   H3T=[ cos(x(3)./a); sin(x(3)./a); -b/a; -1;   0];
+   H4T=[ cos(x(3)./a); sin(x(3)./a);  b/a;  0;  -1];
  
   %slips
-  s14=H(1,:)*x_two;
-  s23=H(2,:)*x_two;
-  s12=H(3,:)*x_two;
-  s34=H(4,:)*x_two;
+  s14=H1*x(6:10);
+  s23=H2*x(6:10);
+  s12=H3*x(6:10);
+  s34=H4*x(6:10);
   
  %% F vector
 % N forces
@@ -68,43 +70,48 @@ P = [Q11   0     Q13/a   0       0;
  N4=(mp/4+mw)*gr;
  
 % \epsilon and \tau coefficients
- epstau=sym('epstau', [8 1]);
+% epstau=sym('epstau', [8 1]);
  
 % R coefficients
- R14=-(epstau(1)*N1 + epstau(4)*N4)*s14;
- R23=-(epstau(2)*N2 + epstau(3)*N3)*s23;
- R12=-(epstau(5)*N1 + epstau(6)*N2)*s12;
- R34=-(epstau(7)*N3 + epstau(8)*N4)*s34;
+ R14=-(1*N1 + 1*N4)*s14;
+ R23=-(1*N2 + 1*N3)*s23;
+ R12=-(3*N1 + 3*N2)*s12;
+ R34=-(3*N3 + 3*N4)*s34;
  
- F14=R14*H(1,:)'/norm(H(1,:));
- F23=R23*H(2,:)'/norm(H(2,:));
- F12=R12*H(3,:)'/norm(H(3,:));
- F34=R34*H(4,:)'/norm(H(4,:)); 
+ norm1=1;
+ norm2=sqrt(2);
+ norm3=sqrt(2+(b/a).^2);
+ norm4=norm3;
+ 
+ F14=R14*H1T./norm1;
+ F23=R23*H2T./norm2;
+ F12=R12*H3T./norm3;
+ F34=R34*H4T./norm4; 
  
  F=F14+F23+F12+F34;
  
  %% Ps matrix --- control function base matrix
-omega=2*pi/T;
-P_vec1=[1 sin(omega*t) cos(omega*t) sin(omega*2*t) cos(omega*2*t)];
-P_s=blkdiag(P_vec1, P_vec1);
+omega=2*pi/T_h;
+P_s=[1 sin(omega*t) cos(omega*t) sin(omega*2*t) cos(omega*2*t) sin(omega*3*t) cos(omega*3*t) 0 0 0 0 0 0 0;
+     0 0 0 0 0 0 0 1 sin(omega*t) cos(omega*t) sin(omega*2*t) cos(omega*2*t) sin(omega*3*t) cos(omega*3*t)];
 
  %% control system
- B=[zeros(2, 3) eye(2)]';
-g=[zeros(5,2); (1/R)*P\B];
-B_lin=g;
+ B=[zeros(3, 2); eye(2)];
+g=[zeros(5,2); (1/R)*inv(P)*B];
+%B_lin=g;
 
 %% output function
-k=[x(1); x(2); x(3)/a];
-C_lin=jacobian(k, x);
-matlabFunction(k, 'file', 'sfun_k', 'vars', {x});
-matlabFunction(B_lin, 'file', 'sfun_B', 'vars', {x});
-matlabFunction(C_lin, 'file', 'sfun_C', 'vars', {x});
+%k=[x(1); x(2); x(3)./a];
+%C_lin=jacobian(k, x);
+%matlabFunction(k, 'file', 'sfun_k', 'vars', {x});
+%matlabFunction(B_lin, 'file', 'sfun_B', 'vars', {x});
+%matlabFunction(C_lin, 'file', 'sfun_C', 'vars', {x});
 matlabFunction(g, 'file', 'sfun_g', 'vars', {x});
-matlabFunction(P_s, 'file', 'sfun_P', 'vars', t);
+matlabFunction(P_s, 'file', 'sfun_P', 'vars', {t});
 
 
  %% generation of A_lin and f
-f=[x_two; P\(-D+F)];
-A_lin=jacobian(f+ g*P_s*lam, x);
-matlabFunction(f, 'file', 'sfun_f', 'vars', {x, epstau});
-matlabFunction(A_lin, 'file', 'sfun_A', 'vars', {x, epstau});
+f=[x(6:10); inv(P)*(-D+F)];
+%A_lin=jacobian(f+ g*P_s*lam, x);
+matlabFunction(f, 'file', 'sfun_f', 'vars', {x});
+%matlabFunction(A_lin, 'file', 'sfun_A', 'vars', {x, epstau});
--- coupled/ode_run2.m
+++ coupled/ode_run2.m
@@ -2,13 +2,8 @@ function [tout, xout] = ode_run2(lambdas)
 
 tstart=0;
 tfinal=4;
-%x0=[0; 0; 0.730*pi/2; zeros(7,1)]; %% dim Ksi= n*m*s
 x0=[0; 0; 0.730*pi/2; zeros(7,1)];
-opt=odeset('MaxStep', 1e-2);
-%epstau=[1 1 1 1 3 3 3 3]';
-    % ode_fun = [f+g Ps lam; A ksi+ BP]
 
-    odefcn=@(t,x) sfun_f(x(1:10))+sfun_g(x(1:10))*sfun_P(t)*lambdas;
-%odefcn=@(t,x) [sfun_f(x(1:10))+sfun_g(x(1:10))*sfun_P(t)*lambdas];
-    
-    [tout, xout] = ode45(odefcn, [tstart, tfinal], x0, opt);
+odefcn=@(t,x) sfun_f(x)+sfun_g(x)*sfun_P(t)*lambdas;
+
+[tout, xout] = ode45(odefcn, [tstart, tfinal], x0);
